cce9ee8f9eb716569559d5ff9c9995a905d0c2f9
Bezeid <23069@supnum.mr>
2025-12-19 11:42:14 +0000

    Initialisation

---
+++ actuator-monitoring/src/main/java/com/example/actuator_monitoring/ActuatorMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.actuator_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorMonitoringApplication.class, args);
+	}
+
+}
---
+++ actuator-monitoring/src/test/java/com/example/actuator_monitoring/ActuatorMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actuator_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
